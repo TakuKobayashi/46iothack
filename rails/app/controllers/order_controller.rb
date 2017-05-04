1a867bab225d233b9bdd464836e8645263acd192
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-05-04 16:45:52 +0900

    QRCodeを表示させるHTMLを作るようにして見た

--- rails/app/controllers/order_controller.rb
+++ rails/app/controllers/order_controller.rb
@@ -29,5 +29,20 @@ class OrderController < BaseController
   end
 
   def result
+    order_recipe = OrderRecipe.find_by(token: cookies[:order_token])
+    url = scan_order_url + "?token=" + order_recipe.token
+    qrcode = RQRCode::QRCode.new(url)
+    png = qrcode.as_png(
+          resize_gte_to: false,
+          resize_exactly_to: false,
+          fill: 'white',
+          color: 'black',
+          size: 320,
+          border_modules: 4,
+          module_px_size: 6,
+          file: nil # path to write
+    )
+    base64_image = Base64.strict_encode64(png.to_s)
+    @image_data = "data:image/png;base64," + base64_image
   end
 end
